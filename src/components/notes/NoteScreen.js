b837687b879ab92e6923cb884bea83021a092988
jantonio2 <jesusrochavedia@gmail.com>
2021-11-09 14:26:30 -0400

    Agregando el resto de tags y estilos en NoteScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,28 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+
+      <div className="notes__content">
+        <input 
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+
+        <textarea
+          placeholder="what happened today?"
+          className="notes__textarea"
+        ></textarea>
+
+        <div className="notes__image">
+          <img 
+            src="https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg"
+            alt="imagen"
+          />
+        </div>
+      </div>
+
     </div>
   )
 }
