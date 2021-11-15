3ad959b5fa30306bf0f70c8c55c7089660182830
jantonio2 <jesusrochavedia@gmail.com>
2021-11-14 20:23:17 -0400

    Activando las notas para su edición

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NotesAppBar } from './NotesAppBar'
+import { useForm } from './../../hooks/useForm';
 
 export const NoteScreen = () => {
+
+  const {active:note} = useSelector(state => state.notes);
+  const [formValues, handleInputChange] = useForm(note);
+
+  const {body, title} = formValues;
+
   return (
     <div className="notes__main-content">
       <NotesAppBar />
@@ -12,19 +20,26 @@ export const NoteScreen = () => {
           placeholder="Some awesome title"
           className="notes__title-input"
           autoComplete="off"
+          value={title}
+          onChange={handleInputChange}
         />
 
         <textarea
           placeholder="what happened today?"
           className="notes__textarea"
+          value={body}
+          onChange={handleInputChange}
         ></textarea>
 
-        <div className="notes__image">
-          <img 
-            src="https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg"
-            alt="imagen"
-          />
-        </div>
+        {
+          note.url &&
+          <div className="notes__image">
+            <img 
+              src="https://neliosoftware.com/es/wp-content/uploads/sites/3/2018/07/aziz-acharki-549137-unsplash-1200x775.jpg"
+              alt="imagen"
+            />
+          </div>
+        }
       </div>
 
     </div>
