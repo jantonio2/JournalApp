00ef5f8fe4ebcde3f8daeebcaa8ad3964e84284c
jantonio2 <jesusrochavedia@gmail.com>
2021-11-18 19:38:16 -0400

    Usando el SDK de Cloudinary para borrar la imagen de prueba

--- src/tests/helpers/fileUpload.test.js
+++ src/tests/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
 import { fileUpload } from './../../helpers/fileUpload';
+import cloudinary from 'cloudinary';
+
+cloudinary.config({ 
+  cloud_name: 'dirvh12z0', 
+  api_key: '573476787932293', 
+  api_secret: '31otQ9JgFj3TBeObk2gRPZOdbtE',
+  secure: true
+});
 
 describe('Pruebas en fileUpload', () => {
   test('debe de cargar un archivo y retornar el URL', async() => {
@@ -9,6 +17,12 @@ describe('Pruebas en fileUpload', () => {
     const url = await fileUpload(file);
 
     expect(typeof url).toBe('string');
+
+    // Borrando la imagen por ID
+    const segments = url.split('/');
+    const imageId = segments[segments.length - 1].replace('.png', '');
+    
+    await cloudinary.api.delete_resources(imageId, {}, () => {});
   });
 
   test('debe de retornar un error', async() => {
