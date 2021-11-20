bc1e748e3b52fc4fa214628f42cd7398f0d8d9ef
jantonio2 <jesusrochavedia@gmail.com>
2021-11-20 18:54:57 -0400

    Pruebas en el componente AppRouter

---
+++ src/tests/components/routers/AppRouter.test.js
@@ -0,0 +1,60 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router';
+import { login } from '../../../actions/auth';
+import { AppRouter } from './../../../routers/AppRouter';
+import { act } from 'react-dom/test-utils';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+jest.mock('../../../actions/auth', () => ({
+  login: jest.fn()
+}));
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+  auth:{},
+  ui: {
+    loading: false,
+    msgError: null
+  },
+  notes: {
+    active: {
+      id: 'test', 
+    },
+    notes: []
+  }
+};
+
+let store = mockStore(initState);
+store.dispatch = jest.fn();
+
+
+describe('Pruebas en <AppRouter />', () => {
+
+  test('debe de llamar el login si estoy autenticado', async() => {
+    
+    let user;
+
+    await act(async() => {
+
+      const auth = getAuth();
+      const userCred = await signInWithEmailAndPassword(auth, 'test@testing.com', '123456');
+      user = userCred.user;
+
+      const wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter>
+            <AppRouter />
+          </MemoryRouter>
+        </Provider>
+      );
+    });
+
+    expect(login).toHaveBeenCalled();
+
+  });
+});
